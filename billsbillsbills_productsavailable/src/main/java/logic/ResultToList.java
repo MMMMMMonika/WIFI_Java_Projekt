3e5a52334cded3cfeb9410972ad35f9698888c9f
Monika <151563741+MMMMMMonika@users.noreply.github.com>
2024-06-23 19:41:49 +0200

    Delete billsbillsbills_productsavailable directory

--- billsbillsbills_productsavailable/src/main/java/logic/ResultToList.java
+++
@@ -1,9 +0,0 @@
-package logic;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-public interface ResultToList<T> {
-    List<T> resultSetToList(ResultSet resultSet) throws SQLException;
-}
